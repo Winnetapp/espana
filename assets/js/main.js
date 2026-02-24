3f8c8e6f9b93e5c01e283066f5c1945f21486eab
Winnetapp <winnetaplicacion@gmail.com>
2026-02-24 23:19:19 +0100

    Actualizar main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -958,6 +958,6 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/espana/service-worker.js")
       .then(() => console.log("Service Worker registrado"))
-      .catch(err => console.log("Error SW", err));
+      .catch(err => console.log("Error al registrar SW", err));
   });
 }
