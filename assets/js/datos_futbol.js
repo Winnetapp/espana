103ee31968806324f7f717d67b8d7fb7c4bfd41a
Winnetapp <winnetaplicacion@gmail.com>
2026-02-25 07:25:55 +0100

    Actualizar datos_futbol.js

--- assets/js/datos_futbol.js
+++ assets/js/datos_futbol.js
@@ -15,17 +15,3 @@ export const equipos_por_liga = {
   "Champions League": ["Real Madrid","Manchester City","Bayern Munich","Liverpool","Paris Saint-Germain","Barcelona","Juventus","Chelsea","Arsenal","Inter Milan"],
   "Mundial de Clubes": ["Real Madrid","Manchester City","Flamengo","Al Hilal","Palmeiras","Chelsea","Al Ahly","Seattle Sounders"]
 };
-
-export const fuerza_equipos = {
-  "Real Madrid": 90,"Barcelona": 88,"Atlético de Madrid": 85,"Sevilla": 80,"Valencia": 78,
-  "Real Sociedad": 78,"Villarreal": 77,"Real Betis": 77,"Athletic Club": 75,"Celta de Vigo": 74,
-  "Getafe": 72,"Espanyol": 72,"Osasuna": 70,"Alavés": 69,"Rayo Vallecano": 68,
-  "Elche": 67,"Mallorca": 66,"Levante": 65,"Girona": 64,"Real Oviedo": 63,
-  "Albacete": 65,"Almería": 66,"Andorra": 64,"Burgos": 63,"Cádiz": 64,"Castellón": 62,"Leganés": 64,"Ceuta": 60,"Córdoba": 60,"Cultural Leonesa": 59,
-  "Deportivo": 61,"Eibar": 63,"Granada": 63,"Huesca": 61,"Málaga": 60,"Mirandés": 62,"Racing Santander": 61,"Real Sociedad B": 63,"Zaragoza": 62,
-  "Sporting": 64,"Las Palmas": 63,"Valladolid": 61,
-  "Manchester City": 92,"Liverpool": 89,"Manchester United": 83,"Arsenal": 87,"Chelsea": 84,"Tottenham": 84,"Newcastle": 81,"Everton": 78,"Aston Villa": 77,"West Ham": 76,"Brighton": 74,"Crystal Palace": 72,"Wolves": 73,"Leeds United": 70,"Burnley": 68,"Brentford": 69,"Fulham": 71,"Bournemouth": 70,"Nottingham Forest": 67,"Sunderland": 66,
-  "Juventus": 85,"Milan": 84,"Inter de Milan": 85,"Napoli": 86,"Roma": 82,"Lazio": 81,"Atalanta": 80,"Fiorentina": 78,"Torino": 76,"Bolonia": 74,"Como": 70,"Udinense": 71,"Parma": 69,"Lecce": 68,"Genoa": 72,"Cagliari": 70,"Verona": 69,"Sassuolo": 72,"Pisa": 67,"Cremonese": 66,
-  "Bayern Munich": 91,"Borussia Dortmund": 88,"RB Leipzig": 85,"Bayer Leverkusen": 83,"Eintracht Frankfurt": 80,"Wolfsburg": 78,"Mönchengladbach": 76,"Freiburg": 75,"Union Berlin": 74,"Werder Bremen": 73,"Stuttgart": 71,"Hoffenheim": 70,"Mainz": 69,"Augsburgo": 68,"San Pauli": 67,"Heidenheim": 66,"Köln": 68,
-  "PSG": 90,"Olympique Lyon": 82,"Mónaco": 80,"Lille": 79,"Angers": 70,"Rennes": 75,"Lens": 74,"Nantes": 73,"Auxerre": 70,"Havre": 69,"Lorient": 68,"Marsella": 77,"Metz": 67,"Niza": 68,"Paris": 72,"Strasbourg": 70,"Toulouse": 71,"Brest": 69
-};
